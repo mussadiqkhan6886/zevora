faf76fbbd247355b7047954ed54e0559bc3b74e5
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-02-04 14:04:56 +0500

    mini header done

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "@/components/customer/Footer";
+import Header from "@/components/customer/Header";
 
 
 export default function RootLayout({
@@ -7,7 +9,9 @@ export default function RootLayout({
 }>) {
   return (
     <>
+    <Header />
     {children}
+    <Footer />
     </>
   );
 }
--- components/customer/Header.tsx
+++ components/customer/Header.tsx
@@ -1,9 +1,34 @@
+'use client';
+
 import React from 'react'
+import {Swiper, SwiperSlide} from "swiper/react"
+import {Autoplay} from "swiper/modules"
+import "swiper/css"
+import { serif } from '@/lib/fonts';
 
 const Header = () => {
   return (
     <header>
-      
+
+      <Swiper
+        modules={[Autoplay]}
+        slidesPerView={1}
+        autoplay={{delay: 2500, disableOnInteraction: false}}
+        loop
+        className={`bg-main text-white text-[13px] ${serif.className}`}
+      >
+        <SwiperSlide className='py-2 text-center tracking-widest'>Delivery charges 190rs Free delivery for orders above 3000rs</SwiperSlide>
+        <SwiperSlide className='py-2 text-center tracking-widest'>All products come with 1 year colour warranty</SwiperSlide>
+      </Swiper>
+
+      <div>
+        <nav>
+
+        </nav>
+        <div>
+
+        </div>
+      </div>
     </header>
   )
 }
