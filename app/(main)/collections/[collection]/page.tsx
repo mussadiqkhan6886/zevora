bae7bb8577247419ead81fae4070c5f8ac6a2355
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-02-04 20:01:21 +0500

    collections done

--- app/(main)/collections/[collection]/page.tsx
+++ app/(main)/collections/[collection]/page.tsx
@@ -1,3 +1,4 @@
+import CardTwo from '@/components/customer/CardTwo'
 import { products } from '@/lib/constants'
 import { serif } from '@/lib/fonts'
 import React from 'react'
@@ -20,12 +21,23 @@ const page = async ({params}: {params: {collection: Promise<string>}}) => {
 
   return (
     <main className='pt-35 max-w-7xl mx-auto'>
-      <h1 className={`${serif.className} capitalize text-3xl`}>{category}</h1>
-      <div>
-        <p>{filtered.length} products</p>
+      <h1 className={`${serif.className} capitalize text-2xl md:text-4xl my-10`}>{category}</h1>
+      <div className='flex justify-between items-center'>
+        <div>
+            <label className="text-zinc-500 text-sm inline-block mr-4">Sort By:</label>
+            <select className="border outline-none text-zinc-600 text-sm border-zinc-300 py-2 px-2">
+                <option>Dates Old to New</option>
+                <option>Dates New to Old</option>
+                <option>Price, Low to High</option>
+                <option>Price, High to Low</option>
+            </select>
+        </div>
+        <p className="text-sm text-zinc-500">{filtered.length} products</p>
       </div>
-      <section>
-
+      <section className='grid gap-6 grid-col-2 md:grid-cols-3 lg:grid-cols-4 my-10'>
+        {filtered.map((item, i) => (
+            <CardTwo key={i} {...item} />
+        ))}
       </section>
     </main>
   )
