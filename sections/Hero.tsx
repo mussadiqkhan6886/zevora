e26824bc62a450582f8f0850c283a89cfbdcc8a4
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-06-20 01:02:10 +0500

    cta done on home page

--- sections/Hero.tsx
+++ sections/Hero.tsx
@@ -1,10 +1,12 @@
 import RotatingText from '@/components/ui/RotatingText'
+import { serif } from '@/lib/fonts'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='h-[100vh] sm:h-[110vh] lg:h-[120vh] xl:h-[110vh] md:px-4'>
+    <section className='h-[100vh] sm:h-[110vh] lg:h-[120vh] xl:h-[110vh] md:px-4 relative'>
          <RotatingText
               texts={['Watches', 'Jewelry', 'Perfumes', 'Rings']}
               mainClassName="px-6 sm:px-2 text-3xl min-[420px]:text-[36px] min-[466px]:text-3xl sm:text-[42px] md:text-6xl lg:text-[93px] md:px-3 text-center max-w-8xl mx-auto my-2 sm:my-5"
@@ -18,6 +20,13 @@ const Hero = () => {
               rotationInterval={2300}
           />
         <Image src="/hero.webp" width={1000} height={1000} priority fetchPriority='high' alt='hero' className='w-full h-[77vh] md:h-[90vh] object-cover object-center rounded-b-4xl' />
+        <div className='absolute left-1/2 top-1/2 -translate-x-1/2 translate-y-[130%]'>
+          <button>
+            <Link className={`${serif.className} px-6 py-2 font-semibold backdrop-blur-sm border rounded-full border-white bg-zinc-50/20 text-black text-center w-full block shadow-xl hover:scale-105 hover:shadow-2xl transition-all duration-300`} href="/collections">
+              Explore Collections
+            </Link>
+          </button>
+        </div>
     </section>
   )
 }
