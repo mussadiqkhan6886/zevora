35b1ed0cbeae29edc5b2dafa1c46d6e26ad0aae2
mussadiqkhan6886 <mussadiqkhan6886@gmail.com>
2026-08-15 18:26:55 +0500

    fixed bug

--- proxy.ts
+++ proxy.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import jwt form "jsonwebtoken"
+import jwt from "jsonwebtoken"
 
 export async function proxy(req: NextRequest) {
   const { pathname } = req.nextUrl;
